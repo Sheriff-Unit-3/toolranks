ebbea973517d6e7d69f22add913c673e98927969
Sheriff_U3 <sheriff_u3@noreply.codeberg.org>
2026-05-22 12:37:22 -0500

    Add hades revisited support

--- init.lua
+++ init.lua
@@ -225,7 +225,7 @@ if core.get_modpath("mcl_tools") then
    toolranks.add_tool("mcl_tools:shovel_netherite")
    toolranks.add_tool("mcl_tools:shovel_diamond")
 
-   -- shears
+   -- Shears
    toolranks.add_tool("mcl_tools:shears")
 end
 
@@ -250,4 +250,43 @@ if core.get_modpath("tech") then
   -- hammers
   toolranks.add_tool("tech:hammer_granite")
   toolranks.add_tool("tech:hammer_basalt")
+end
+
+-- hades revisited support
+if core.get_modpath("hades_core") then
+  -- Pickaxe
+  toolranks.add_tool("hades_core:pick_wood")
+  toolranks.add_tool("hades_core:pick_stone")
+  toolranks.add_tool("hades_core:pick_iron")
+  toolranks.add_tool("hades_core:pick_steel")
+  toolranks.add_tool("hades_core:pick_bronze")
+  toolranks.add_tool("hades_core:pick_mese")
+  toolranks.add_tool("hades_core:pick_prism")
+
+  -- Shovel
+  toolranks.add_tool("hades_core:shovel_wood")
+  toolranks.add_tool("hades_core:shovel_stone")
+  toolranks.add_tool("hades_core:shovel_iron")
+  toolranks.add_tool("hades_core:shovel_steel")
+  toolranks.add_tool("hades_core:shovel_bronze")
+  toolranks.add_tool("hades_core:shovel_mese")
+  toolranks.add_tool("hades_core:shovel_prism")
+
+  -- Axe
+  toolranks.add_tool("hades_core:axe_wood")
+  toolranks.add_tool("hades_core:axe_stone")
+  toolranks.add_tool("hades_core:axe_iron")
+  toolranks.add_tool("hades_core:axe_steel")
+  toolranks.add_tool("hades_core:axe_bronze")
+  toolranks.add_tool("hades_core:axe_mese")
+  toolranks.add_tool("hades_core:axe_prism")
+
+  -- Sword
+  toolranks.add_tool("hades_core:sword_wood")
+  toolranks.add_tool("hades_core:sword_stone")
+  toolranks.add_tool("hades_core:sword_iron")
+  toolranks.add_tool("hades_core:sword_steel")
+  toolranks.add_tool("hades_core:sword_bronze")
+  toolranks.add_tool("hades_core:sword_mese")
+  toolranks.add_tool("hades_core:sword_prism")
 end
